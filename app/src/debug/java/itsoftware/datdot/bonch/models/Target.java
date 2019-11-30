41a4ccf7b2c0153915c3a6675d181f3035bb4485
Dmitry Kabarukhin <thedimzone99@gmail.com>
2019-11-30 17:59:49 +0300

    right

---
+++ app/src/debug/java/itsoftware/datdot/bonch/models/Target.java
@@ -0,0 +1,12 @@
+package itsoftware.datdot.bonch.models;
+
+import java.util.Map;
+
+public class Target {
+    private String id;
+    private String type; // "Archicture", "Bar', etc
+    private int age;
+    private int level; //1, 2, etc
+    //private Geo... coordinates;
+    //private Map<String, >
+}
